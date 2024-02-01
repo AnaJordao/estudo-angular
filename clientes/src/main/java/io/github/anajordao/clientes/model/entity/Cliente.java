66a9c31e41d2417ab77905b6dec2be9d30ee9a57
AnaJordao <acjpjvjp@gmail.com>
2024-02-01 19:14:35 -0300

    CRUD do cliente e tratamento de erros por mensagens feitos

--- clientes/src/main/java/io/github/anajordao/clientes/model/entity/Cliente.java
+++ clientes/src/main/java/io/github/anajordao/clientes/model/entity/Cliente.java
@@ -2,6 +2,8 @@ package io.github.anajordao.clientes.model.entity;
 
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Column;
@@ -10,6 +12,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -29,12 +33,15 @@ public class Cliente {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
-    @Column(name = "data_cadastro")
+    @Column(name = "data_cadastro", updatable = false)
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro; 
 
--- clientes/src/main/java/io/github/anajordao/clientes/rest/ClienteController.java
+++ clientes/src/main/java/io/github/anajordao/clientes/rest/ClienteController.java
@@ -2,14 +2,20 @@ package io.github.anajordao.clientes.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.anajordao.clientes.model.entity.Cliente;
 import io.github.anajordao.clientes.model.repository.ClienteRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -22,9 +28,45 @@ public class ClienteController {
         this.repository = repository;
     }
 
+    // CREATE   
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar(@RequestBody Cliente cliente) {
+    public Cliente salvar(@RequestBody @Valid Cliente cliente) {
         return repository.save(cliente);
+    }
+
+    // READ BY ID
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id){
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
+    }
+
+    // DELETE
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id){
+        repository
+            .findById(id)
+            .map(cliente -> {
+                repository.delete(cliente);
+                return Void.TYPE;
+            })
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
+    }
+    
+    // UPDATE
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado){
+        repository
+            .findById(id)
+            .map(cliente -> {
+                cliente.setNome(clienteAtualizado.getNome());
+                cliente.setCpf(clienteAtualizado.getCpf());
+                return repository.save(clienteAtualizado);
+            })
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado."));
     }
 }

23ccbc407ed927d3af4b20abc071c9a7dc0589e2
AnaJordao <acjpjvjp@gmail.com>
2024-02-14 19:08:34 -0300

    crud de clientes do front integrado com o back terminado

--- clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,32 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClientesService } from '../../clientes.service';
 import { HttpErrorResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
   styleUrl: './clientes-form.component.css'
 })
-export class ClientesFormComponent {
+export class ClientesFormComponent implements OnInit {
     cliente!: Cliente;
     success: boolean = false;
     errors: string[] | undefined;
+    id! : number;
 
-    constructor(private service : ClientesService){
+    constructor(private service : ClientesService, 
+                private router : Router,
+                private activatedRoute : ActivatedRoute){
         this.cliente = new Cliente();
     }
 
+    ngOnInit(): void {
+        let params = this.activatedRoute.params;
+        params.subscribe(urlParams => {
+            this.id = urlParams['id'];
+            if(this.id){
+                this.service
+                    .getClienteById(this.id)
+                    .subscribe(res => this.cliente = res, 
+                                errorRes => this.cliente = new Cliente())
+            }
+        })
+    }
+
+    voltarParaListagem(){
+        this.router.navigate(['/clientes-lista'])
+    }
+
     onSubmit(){
-        this.service
-            .salvar(this.cliente)
-            .subscribe(response => {
-                this.success = true;
-                this.errors = [];
-                this.cliente = response;
-            }, errorRes => {
-                this.success = false;
-                this.errors = errorRes.error.errors;
-            })
+        if(this.id){
+            this.service
+                .atualizar(this.cliente)
+                .subscribe(res => {this.success = true;
+                                    this.errors = [];
+                }, erroRes => {
+                    this.errors = ['Erro ao atualizar o cliente.']
+                });
+        }else{
+            this.service
+                .salvar(this.cliente)
+                .subscribe(response => {
+                    this.success = true;
+                    this.errors = [];
+                    this.cliente = response;
+                }, errorRes => {
+                    this.success = false;
+                    this.errors = errorRes.error.errors;
+                })
+        }
     }
 }
---
+++ clientes-app/src/app/clientes/clientes-lista/clientes-lista.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { ClientesService } from '../../clientes.service';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'app-clientes-lista',
+    templateUrl: './clientes-lista.component.html',
+    styleUrl: './clientes-lista.component.css'
+})
+export class ClientesListaComponent implements OnInit{
+    clientes: Cliente[] = [];
+    clienteSelecionado! : Cliente;
+    mensagemSucesso: string = '';
+    mensagemErro: string = '';
+
+    constructor(private service: ClientesService, private router : Router){ // para se conectar a api
+        
+    }
+
+    ngOnInit(): void { // ao inicializar este componente
+        this.service
+            .getClientes()
+            .subscribe(res => this.clientes = res);
+    }
+
+    novoCadastro(){
+        this.router.navigate(['/clientes-form'])
+    }
+
+    preparaDelecao(cliente : Cliente){
+        this.clienteSelecionado = cliente;
+    }
+
+    deletarCliente(){
+        this.service
+            .deletar(this.clienteSelecionado)
+            .subscribe(
+                res => {
+                    this.mensagemSucesso = 'Cliente deletado com sucesso.'
+                    this.ngOnInit();
+                },
+                erro => this.mensagemErro = 'Ocorreu um erro ao deletar o cliente.'
+            )
+    }
+}

6f835979d28b24881bc3434ebdaa29537b070be4
AnaJordao <acjpjvjp@gmail.com>
2024-02-19 21:43:26 -0300

    Resolver bug da configuração do Spring Security

--- clientes/src/main/java/io/github/anajordao/clientes/config/SecurityConfig.java
+++ clientes/src/main/java/io/github/anajordao/clientes/config/SecurityConfig.java
@@ -1,15 +1,58 @@
 package io.github.anajordao.clientes.config;
 
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @EnableWebSecurity
-@EnableResourceServer
-@EnableAuthorizationServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 // public class SecurityConfig extends AuthorizationServerConfigurerAdapter {
-public class SecurityConfig implements WebMvcConfigurer {
+public class SecurityConfig {
+
     
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new ShopmeUserDetailsService();
+    }
+ 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception{
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+
+        AuthenticationManager authenticationManager = authenticationManagerBuilder.build();
+
+        http
+            .cors().and()
+            .csrf().disable().authorizeRequests()
+            .requestMatchers(HttpMethod.POST, "url/***")
+                .permitAll()
+            .anyRequest().authenticated().and()
+            .authenticationManager(authenticationManager)
+            .sessionManager()
+            .sesseionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.headers().frameOptions().disable();
+
+        return http.build();
+    }
 }
